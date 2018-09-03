fb6e4d0c0475e867a13a9248ece1789efd8a4a8c
JckXia <ys3xia@edu.uwaterloo.ca>
2018-09-03 14:57:43 -0400

    Loaded Stock Tick Symbols,implemeneted a searching system

--- FrontEnd/ui/src/App.js
+++ FrontEnd/ui/src/App.js
@@ -3,15 +3,22 @@ import logo from './logo.svg';
 import './css/App.css';
 import SideNav from './SideNavigation'
 import CurrentStocks from './CurrentStocks'
+ import data from './Ticks.json'
 class App extends Component {
+  state={
+    Ticker:data
+  }
+
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
       <span ><span className="credit">Powered By React.js</span><img src={logo} className="App-logo" alt="logo" /></span>
     <h1 className="App-title">YearOfTheBull</h1>
     </header>
-          <SideNav/>
+          <SideNav   Ticker={this.state.Ticker}/>
 
         <CurrentStocks/>
       </div>
--- FrontEnd/ui/src/SideNavigation.js
+++ FrontEnd/ui/src/SideNavigation.js
@@ -1,24 +1,36 @@
 import React from 'react'
+import escapeRegExp from 'escape-string-regexp'
 import {slide as Menu } from 'react-burger-menu'
 import './css/SideNavigation.css'
-class SideNav extends React.Component{
 
+class SideNav extends React.Component{
+  state={
+    query:""
+  }
+  updateQuery=(query)=>{
+    this.setState({query:query.trim()})
+  }
   render(){
+
+  var ShowTicks;
+   if(this.state.query){
+    const match=new RegExp(escapeRegExp(this.state.query),'i')
+     ShowTicks=this.props.Ticker.filter((stock)=>match.test(stock.AABA));
+   }else{
+     ShowTicks=this.props.Ticker;
+   }
 
       return(
                <Menu>
-           
-              <input role="menu" type="text" className="filter"  placeholder="Ex:6 flags"/>
-            <a className="stock" onClick={()=>{console.log('Hi')}}>GOOGL</a>
-          <div className="space"> </div>
-
-        <a className="stock" onClick={()=>{console.log('Hi')}}>MSFT</a>
-        <div className="space"> </div>
-
-      <a className="stock" onClick={()=>{console.log('Hi')}}>APPL</a>
-      <div className="space"> </div>
-
-
+               <h1>Stocks</h1>
+              <input role="menu" onChange={event=>this.updateQuery(event.target.value)}type="text" className="filter"  placeholder="Ex:6 flags"/>
+            {ShowTicks.map((data,i)=>
+              <div key={i}>
+              <a className="stock">{data.AABA}</a>
+              <div className="space"></div>
+              </div>
+            )
+            }
          </Menu>
 
       );
